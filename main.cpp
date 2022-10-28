b838f1e38ffdd8c86dfe37de9de0f19e80facff5
omdia <116859416+omdia@users.noreply.github.com>
2022-10-28 11:08:47 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string firstName = "Rizky";
+    string middleName1 = "Dwi";
+    string middleName2 = "Putra";
+    string lastName = "Sidik";
+    string fullName = firstName + " " + middleName1 + " " + middleName2 + " " + lastName;
+
+    cout << "Nama lengkap saya adalah " << fullName;
+
+    return 0;
+}
